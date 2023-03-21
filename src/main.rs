7a141accaf15d1f2f846d4a9dcaded215b99ba5b
Joshua Okoro <josidbobo@gmail.com>
2023-03-21 08:44:14 +0100

    chore: update on file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().unwrap();
-    println!("The key is {}", key);
+    let value = arguments.next().unwrap();
+    println!("The key is {} and the value is {}", key, value);
+
+    let contents = format!("{}\t{}\n", key, value);
+    let write_result = std::fs::write("kv.db", contents);
 }
